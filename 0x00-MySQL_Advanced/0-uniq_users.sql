487a3c5933c32193620882495d331b692cabd94f
KENC0DE <kenansameseret@gmail.com>
2024-03-20 21:14:11 +0530

    adv sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,10 +1,6 @@
 -- Creates a table called users.
-CREATE DATABASE IF NOT EXISTS holberton;
-
-USE holberton;
-
 CREATE TABLE IF NOT EXISTS users (
 	id INT AUTO_INCREAMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
-)
+);
