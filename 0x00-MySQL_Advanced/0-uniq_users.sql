8a5999f4bdd9dadf534c3d6bda8bd64e36585ae9
KENC0DE <kenansameseret@gmail.com>
2024-03-20 21:15:27 +0530

    adv sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
--- Creates a table called users.
+-- Creates users table.
 CREATE TABLE IF NOT EXISTS users (
-	id INT AUTO_INCREAMENT PRIMARY KEY,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
