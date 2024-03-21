ecffa68e1e592f6debcbfdcbecff3d1d95cb2b96
KENC0DE <kenansameseret@gmail.com>
2024-03-21 19:13:17 +0530

    sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,5 @@
 -- filter by glam rock
-SELECT band_name,
-    CASE
-        WHEN split = NULL THEN 2022 - formed
-        ELSE split - formed
-    END AS lifespan
+SELECT band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style = '%Glam rock%'
 ORDER BY lifespan DESC;
