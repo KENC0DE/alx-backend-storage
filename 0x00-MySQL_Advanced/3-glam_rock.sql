8fa94b464c8b6838df23bb3ce6104d5c1002d49d
KENC0DE <kenansameseret@gmail.com>
2024-03-21 19:00:50 +0530

    sql

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,9 @@
+-- filter by glam rock
+SELECT band_name,
+    CASE
+        WHEN split = 0 THEN 2022 - formed
+        ELSE split - formed
+    END AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC;
