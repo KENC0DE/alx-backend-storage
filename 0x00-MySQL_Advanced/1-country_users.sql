9cdc820cfc24668927f8a33ca06bcb8a32ed105f
KENC0DE <kenansameseret@gmail.com>
2024-03-21 17:51:05 +0530

    sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- create table with country users
 CREATE TABLE IF NOT EXISTS users (
-        id INT NOT NULL AUTO_INCREAMENT,
-        email VARCHAR(255) NOT NULL UNIQUE,
-        name VARCHAR(255),
-        country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
-        PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREAMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
 );
