77e8ebc0b4de389f6dad4d01c78a9e92972903d3
KENC0DE <kenansameseret@gmail.com>
2024-03-26 19:05:39 +0530

    NoSQL

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 """
-NoSQL -Python3
+Return all students sorted by average score
+Prototype: def top_students(mongo_collection):
+The top must be ordered
+average score retuens with key = averageScore
 """
 
 
 def top_students(mongo_collection):
-    """Get all students from the collection"""
-    all_students = mongo_collection.find()
-
-    students_with_avg = []
-    for student in all_students:
-        scores = student['scores']
-        avg_score = sum(score['score'] for score in scores) / len(scores)
-        student['averageScore'] = avg_score
-        students_with_avg.append(student)
-
-    sorted_students = sorted(students_with_avg,
-                             key=lambda x: x['averageScore'], reverse=True)
-
-    return sorted_students
+    """
+    Prototype: def top_students(mongo_collection):
+    Returns all students sorted by average score
+    """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+            {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort":
+            {
+                "averageScore": -1
+            }
+        }
+    ])
