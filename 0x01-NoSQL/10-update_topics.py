64c47a2b4fbb0473441f5e4a9d116406981d15eb
KENC0DE <kenansameseret@gmail.com>
2024-03-26 17:40:25 +0530

    NoSQL

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+NoSQL -Python
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
+    """
+    target = {"name": name}
+    alter = {"$set" : {"topics": topics}}
+    mongo_collection.update_many(target, alter)
