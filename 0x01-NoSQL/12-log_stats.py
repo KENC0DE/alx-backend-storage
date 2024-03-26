10a98eb34eb9029cd851e83ff41fb2ab1e1b0ed0
KENC0DE <kenansameseret@gmail.com>
2024-03-26 21:26:04 +0530

    fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,24 +6,15 @@ if __name__ == '__main__':
     from pymongo import MongoClient
 
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client['logs']
-    coll = db['nginx']
+    nginx = client.logs.nginx
 
-    total_count = coll.count_documents({})
-    get_count = coll.count_documents({"method": "GET"})
-    post_count = coll.count_documents({"method": "POST"})
-    put_count = coll.count_documents({"method": "PUT"})
-    patch_count = coll.count_documents({"method": "PATCH"})
-    delete_count = coll.count_documents({"method": "DELETE"})
-    get_status = coll.count_documents({"method": "GET", "path": "/status"})
+    print(nginx.count_documents({}), 'logs')
+    print('Methods:')
 
-    message = f"""{total_count} logs
-Methods:
-    method GET: {get_count}
-    method POST: {post_count}
-    method PUT: {put_count}
-    method PATCH: {patch_count}
-    method DELETE: {delete_count}
-{get_status} status check"""
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for meth in methods:
+        coutned = nginx.count_documents({'method': meth})
+        print('\tmethod {}: {}'.format(meth, coutned))
 
-    print(message)
+    count_stat = nginx.count_documents({'method': 'GET', 'path': '/status'})
+    print('{} status check'.format(count_stat))
